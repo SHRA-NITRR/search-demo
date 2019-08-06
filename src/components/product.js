997e0949c84a5bdb9b474a265df0bb20d1cf0e82
unknown <you@example.com>
2019-08-07 00:33:57 +0530

    cleanup

--- src/components/product.js
+++ src/components/product.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const Product=(props)=>{
-    const {category, name, price, stocked}=props.productDetails;
+    const {name, price, stocked}=props.productDetails;
     return( 
             <div className='panel panel-default'>
                <div className="panel-body product-details">
